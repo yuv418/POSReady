dc1825686135cd410decb49425752aea9b5b0350
cdknight <enterprisebalaji@hotmail.com>
2017-09-06 20:02:26 -0400

    added to posxmlreader: NOT DONE, must finish
    Change-Id: Ida793577e8db265a9a461cf93acc95313cc83d9f

--- src/config/POSXMLReader.java
+++ src/config/POSXMLReader.java
@@ -7,12 +7,18 @@ package config;
 import javax.xml.parsers.DocumentBuilder; 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 //other imports
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import org.xml.sax.SAXException;
 
 public class POSXMLReader {
@@ -47,5 +53,29 @@ private Document parsed;
 		}
 	}
 	
+	
+	public String getResultById(String xmlpath) {
+		/*
+		  Returns a string based on custom path specification:  
+		  
+		  / is base of document
+		  /debug/boolean where xml is <debug><boolean>RETURNS WHAT IS HERE</boolean></debug> returns RETURNS WHAT IS HERE
+		  /a/b/c will return e where xml is <a><b><c>e</c></b></a>
+		 */
+		String[] path_id = xmlpath.split(":");
+		String path = path_id[0];
+		String id = path_id[1];
+		String[] paths = path.split("/");
+		
+		NodeList nl = parsed.getElementsByTagName(paths[0]);
+		Node e = nl.item(0);
+		NodeList childNodes = e.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node cn = childNodes.item(i);
+			if 
+		}
+		
+		
+	}
 	
 }
