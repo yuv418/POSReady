1ceed6053fe0e8816b24844e699ec62181b7395c
cdknight <enterprisebalaji@hotmail.com>
2017-09-23 18:22:12 -0400

    finished base class posemployee
    Change-Id: I2771dc0fa67fb139b7f1bbdf48f4f53756440f5d

--- src/employee_implementations/POSEmployee.java
+++ src/employee_implementations/POSEmployee.java
@@ -2,6 +2,7 @@ package employee_implementations;
 import java.sql.*;
 
 import config.POSDebugConfig;
+import config.POSSQLConfig;
 
 public class POSEmployee {
 /****
@@ -9,28 +10,37 @@ public class POSEmployee {
  */
 	
 private Connection mariadb_default; 
-	
-public POSEmployee(){
+private String user_type; //table name of user category table in database: pos_users, pos_admin_users, pos_top_admin_users
+private String username;
+public POSEmployee(String utype, String u_name){
 /*
  * Default POSEmployee constructor
  * 
  * 	
  */
-
+	user_type = utype;
+	u_name = username; 
   try {
-	mariadb_default = DriverManager.getConnection("jdbc:mariadb://localhost:3306/posready", "default_u", "letmeinmysql");
+	mariadb_default = DriverManager.getConnection("jdbc:mariadb://localhost:3306/posready", POSSQLConfig.getSQLUsername(), POSSQLConfig.getSQLPassword());
   } 
   catch (SQLException e) {
 	if (POSDebugConfig.console_debug()) {
-		System.err.println("posready: fatal, cannot connect to posready default database"); // TODO Switch to automated printer  
-		  
+		System.err.println("debug: fatal, cannot connect to posready default database"); // TODO Switch to automated printer  
+		e.printStackTrace();
+		System.exit(-1);
 	}
 	else if(!POSDebugConfig.console_debug()) {
 		System.err.println("Sorry, we couldn't connect to the POSReady Database at this time."); //the posready database is a glorified name for mariadb. 
+		System.exit(-1);
 	}
   }
-
 }
+
+public String getUsername() {
+	return username;
+}
+
+
 
 
 
