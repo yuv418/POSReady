24158f47aee80481420fa87a75e0e88e29914730
cdknight <enterprisebalaji@hotmail.com>
2017-09-30 21:33:40 -0400

    tests.
    Change-Id: I5609785146541655199e600d318759d2381bdc2a

--- src/tests/TstCreateUser.java
+++ src/tests/TstCreateUser.java
@@ -1,12 +1,13 @@
 package tests;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import auth.CreateUser;
 import auth.EmployeeNotCreatedException; 
 public class TstCreateUser {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, EmployeeNotCreatedException {
+	public static void main(String[] args) throws FileNotFoundException, IOException, EmployeeNotCreatedException, SQLException {
 		CreateUser v = new CreateUser("billy", "bob", "joe", "billy", "honk", "pos_top_admin_users"); 
 		v.writeInfo();
 
