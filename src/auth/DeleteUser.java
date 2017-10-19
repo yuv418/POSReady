313b9179bc4a7cdbccda72963a9476ea324129b4
cdknight <enterprisebalaji@hotmail.com>
2017-10-19 19:24:15 -0400

    Added DeleteUser + code
    Change-Id: Ie2542dac1787bf50fa0dd81099622f8d4fe05eaf

--- src/auth/DeleteUser.java
+++ src/auth/DeleteUser.java
@@ -2,4 +2,13 @@ package auth;
 
 public class DeleteUser {
 	//delete a user
+	public DeleteUser(String username){
+		//code goes here
+	}
+	
+	public DeleteUser(int userid){
+		//code goes here
+	}
+	
+	
 }
