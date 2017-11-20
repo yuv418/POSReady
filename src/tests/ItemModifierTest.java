0cdd2e50fcbff82c688e069a3c034ec7ee95caec
cdknight <enterprisebalaji@hotmail.com>
2017-11-19 20:00:58 -0500

    created and tested ItemModifierTest
    everything is working fine.

--- src/tests/ItemModifierTest.java
+++ src/tests/ItemModifierTest.java
@@ -1,7 +1,13 @@
 package tests;
 
+import java.sql.SQLException;
+
+import items.ItemModifier;
+
 public class ItemModifierTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ItemModifier itmod_test = new ItemModifier(1, 0.11);
+		itmod_test.modify_item();
 		
 	}
 }
