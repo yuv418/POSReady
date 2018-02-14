e9e7c6a7132d57524adb361ec462fd4e2e5fd7ba
cdknight <enterprisebalaji@hotmail.com>
2018-02-13 22:24:42 -0500

    CreateItemView - worked on it a little more, will next commit for multithreading validator (which was removed earlier on)
    Signed-off-by: cdknight <enterprisebalaji@hotmail.com>

--- src/views/CreateItemView.java
+++ src/views/CreateItemView.java
@@ -1,17 +1,17 @@
 package views;
 import java.awt.event.ActionListener;
 import java.sql.*;
-import items.POSItem;
+import items.CreateItem;
 
 import javax.swing.*;
 import java.awt.event.*;
 
 public class CreateItemView extends View {
-    private POSItem item_tbc;
+    private CreateItem item_tbc;
     private JFrame frame;
     private JPanel panel;
+    private JTextField item_name;
     private JTextField descr;
-    private JTextField id;
     private JTextField price;
     private JButton submit_btn;
     public CreateItemView(String env_type) {
@@ -24,17 +24,40 @@ public class CreateItemView extends View {
         //configure panel
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); //y axis box layout
 
+        item_name = new JTextField(); //one line only of JTextField
         descr = new JTextField(5); //we'll have to limit this to 300 characters
-        id = new JTextField();
         price = new JTextField();
 
         submit_btn = new JButton("Submit");
         submit_btn.addActionListener(new CreateItemViewListener());
+
+        panel.add(item_name);
+        panel.add(descr);
+        panel.add(price);
+
+        frame.add(panel);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public class CreateItemViewListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
+            //fetch information
+            String item_name_str = item_name.getText();
+            String descr_str = descr.getText();
 
+            double price_double = Double.parseDouble(price.getText());
+
+            try{
+                item_tbc = new CreateItem(item_name_str, descr_str, price_double);
+            }
+            catch (ClassNotFoundException cnfe){
+                JOptionPane.showMessageDialog(panel, "Sorry, but the item cannot be created since POSReady cannot access the database. Please check your installation and try again.");
+            }
+            catch (SQLException se){
+                JOptionPane.showMessageDialog(panel, "Sorry, but the POSReady database cannot be accessed at this time. Please check your POSReady installation, try again later, or make sure you are not using an unstable build.");
+            }
         }
     }
 }
